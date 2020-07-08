769598ca8c4046903fa846f9e9bdbde3dd69f5ad
satyak24 <devops1.satya@gmail.com>
2020-07-08 11:03:28 +0530

    pythonfile added

---
+++ add.py
@@ -0,0 +1,22 @@
+
+a = int(input("enter a value: "))
+b = int(input("enter b value: "))
+def add():
+    c = a+b
+    print("addition of two nums is :", c)
+
+
+def sub():
+    c = a-b
+    print("substraction of two num is :", c)
+
+
+def mul():
+    c = a*b
+    print("multiplication of of two num is :", c)
+
+
+
+add()
+sub()
+mul()
